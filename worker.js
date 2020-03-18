c44abb16980c3d131f1219f54306938cfb5ecb13
guest271314 <guest271314@gmail.com>
2020-03-18 01:16:05 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,28 +1,22 @@
 // AudioWorkletStream
 // Stream audio from Worker to AudioWorklet (POC)
 // guest271314 2-24-2020
-import {
-  CODECS
-}
-from "./codecs.js";
 let port;
 onmessage = async e => {
-  "use strict";
+  'use strict';
   if (!port) {
     [port] = e.ports;
     port.onmessage = event => postMessage(event.data);
   }
   const {
-    codecs: [mimeCodec],
     urls: [url],
   } = e.data;
-  const {
-    default: processStream
-  } = await
-  import (CODECS.get(mimeCodec));
   const response = await fetch(url);
   const readable = response.body;
-  port.postMessage({
-    readable, processStream: processStream.toString()
-  }, [readable]);
+  port.postMessage(
+    {
+      readable,
+    },
+    [readable]
+  );
 };
