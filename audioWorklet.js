af33caca63fdd4e4e3f8b93d6d0329cddd4097b0
guest271314 <guest271314@gmail.com>
2020-07-23 08:04:15 +0000

    Update audioWorklet.js
    For no gaps or glitches and to maintain this.index (data read in message event handler)  > this.offset (data storeed in Uint8Array) accumulate 344 * 512 * 1.5 of data before resuming audio context and to

--- audioWorklet.js
+++ audioWorklet.js
@@ -1,29 +1,23 @@
+import { CODECS } from './codecs.js';
 class AudioDataWorkletStream extends AudioWorkletProcessor {
   constructor(options) {
     super(options);
-    if (options.processorOptions) {
-      Object.assign(this, options.processorOptions);
-    }
-    this.port.onmessage = this.appendBuffers.bind(this);
-    this.processPerSecond = 0;
-    this.bool = false;
-    this.minSamples = 1024;
-  }
-  async appendBuffers({ data: { channel0, channel1 } }) {
-    this.buffers.set(this.i, {
-      channel0,
-      channel1,
+    Object.assign(this, options.processorOptions, {
+      uint8: new Uint8Array(290689440 - 44),
     });
-    ++this.i;
-    if (this.i === this.minSamples) {
-      console.log(
-        'this.buffers.size:' + this.buffers.size,
-        currentTime,
-        currentFrame
-      );
-      this.port.postMessage({
-        start: true,
-      });
+    globalThis.console.log(currentTime, currentFrame, this.index, this.offset);
+    this.port.onmessage = this.appendBuffers.bind(this);
+  }
+  async appendBuffers({ data: value }) {
+    for (let i = !this.index ? 44 : 0; i < value.length; i++) {
+      this.uint8[this.index++] = value[i];
+      // accumulate 344 * 512 * 1.5 of data
+      // to avoid glitches at beginning of playback
+      // maintain this.index > this.offset
+      // to avoid gaps during playback
+      if (this.index === 344 * 512 * 1.5) {
+        this.port.postMessage({ start: true });
+      }
     }
   }
   endOfStream() {
@@ -34,56 +28,22 @@ class AudioDataWorkletStream extends AudioWorkletProcessor {
     });
   }
   process(inputs, outputs) {
-    if (!this.bool) {
-      ++this.processPerSecond;
-    }
-    if (currentTime >= 0.9985 && !this.bool) {
-      console.log(this.processPerSecond, currentTime);
-      this.bool = true;
-    }
-    if (!this.processStarted) {
-      this.processStarted = true;
-      console.log('process() first call', currentTime, this.buffers.size);
-    }
-    if (currentTime > 0.9 && this.buffers.size === 0 && this.n === this.i) {
-      console.log(
-        this.i,
-        this.n,
-        this.buffers.size
-      );
+    if (this.offset === this.uint8.length) {
       this.endOfStream();
       return false;
     }
-    let channel0, channel1;
-    // handle TypeError: Cannot destructure property 'channel0' of 'this.buffers.get(...)' as it is undefined.
-    // while this.buffers.size === 0, this.readable.locked, this.writable.locked
-    // can occur where minSamples < 64 in appendBuffers()
-    try {
-      ({ channel0, channel1 } = this.buffers.get(this.n));
-      // glitches can occur when sample frame size is less than 128
-      // true at most once, if Float32Array's with length less than 128 in overflow array, last of samples
-      if (
-        (channel0 && channel0.length < 128) ||
-        (channel1 && channel1.length < 128)
-      ) {
-        console.log(
-          channel0.length,
-          channel1.length,
-          currentTime,
-          currentFrame,
-          this.buffers.size
-        );
+    const channels = outputs.flat();
+    const uint8 = new Uint8Array(512);
+    for (let i = 0; i < 512; i++, this.offset++) {
+      if (this.offset === this.uint8.length) {
+        console.log(this.uint8);
+        break;
       }
-    } catch (e) {
-      console.error(e, this.buffers.size, this.i, this.n);
-      // return true until this.buffers.size > 0
-      return true;
+      uint8[i] = this.uint8[this.offset];
     }
-    const [[outputChannel0], [outputChannel1]] = outputs;
-    outputChannel0.set(channel0);
-    outputChannel1.set(channel1);
-    this.buffers.delete(this.n);
-    ++this.n;
+    const uint16 = new Uint16Array(uint8.buffer);
+    CODECS.get(this.codec)(uint16, channels);
+    console.log(this.offset, this.index);
     return true;
   }
 }
